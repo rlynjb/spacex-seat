d3711117054cc3545e083a0b3c18955a1d230e50
rlyn <rlynjb@gmail.com>
2023-03-31 06:45:32 -0700

    added mutation resolvers to login user, set auth, and use sqlite as db

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -7,6 +7,8 @@ import { LaunchAPI } from "./datasources/launch.js";
 import { UserAPI } from "./datasources/user.js";
 import { resolvers } from "./resolvers.js";
 
+import isEmail from "isemail";
+
 const store = createStore();
 
 const server = new ApolloServer({
@@ -15,7 +17,20 @@ const server = new ApolloServer({
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store }),
-  })
+  }),
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+
+    if (!isEmail.validate(email)) return { user: null };
+
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+
+    return { user: { ...user.dataValues } };
+  },
 });
 
 server.listen().then(() => {
